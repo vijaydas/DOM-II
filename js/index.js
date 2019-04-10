12826c195c78dd24e7f439360f8ea3368a5dc706
Vijay Das <vijaytdas@gmail.com>
2019-04-10 07:42:35 -0500

    add bus animated across header

--- js/index.js
+++ js/index.js
@@ -1,7 +1,33 @@
 // Your code goes here
-const btn = document.querySelectorAll('.btn');
-console.log(btn);
 
-btn.addEventListener('click', function(event) {
-  console.log('currentTarget:', event.currentTarget.innerHTML)
+// sign-up button mouseover
+ const btn = document.querySelectorAll('.btn');
+ console.log(btn);
+
+ btn[0].addEventListener("mouseover", function(event) {
+   console.log(event.target)
+   event.target.style.color = "orange";
+ });
+
+
+ btn[0].addEventListener("mouseover", function(event) {
+    console.log(event.target)
+    event.target.style.color = "crimson";
   });
+
+ 
+ btn[0].addEventListener("mouseover", function(event) {
+    console.log(event.target)
+    event.target.style.color = "blue";
+  });
+  
+// bus animation
+
+ const busInImg = document.querySelector(".hidden");
+ window.addEventListener("load", function(event) {
+    console.log(busInImg.classList);
+    busInImg.classList.remove("hidden");
+    busInImg.classList.add("slideRight");
+ });
+
+
